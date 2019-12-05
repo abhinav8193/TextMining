8e7f2299c7b44bb4a8c3187e064970bc52701281
Abhinav <abhinav@gmail.com>
2019-12-05 22:44:43 +0530

    Added new classfile Image Analysis

---
+++ DataFrame/Image-analysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+df=pd.read_csv("filename.csv")
+
+class ImageProcessing(object):
+ 	"""docstring for ClassName"""
+ 	def __init__(self, arg):
+ 		super(ClassName, self).__init__()
+ 		self.arg = arg
+ 		 TextProcessing(){
+	
+}
